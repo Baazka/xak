ae104f170431d7dc1b8a5d9936eb71d08762fb2d
baazka <baazka0705@gmail.com>
2026-02-04 11:39:29 +0800

    -del

--- src/lib/recaptcha.ts
+++
@@ -1,5 +0,0 @@
-export async function getRecaptchaToken(action: string) {
-  if (!window.grecaptcha) return null;
-
-  return await window.grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!, { action });
-}
