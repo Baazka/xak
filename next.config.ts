7dc86fac9b5b2340f5f3c68a27ed3b995f27f76e
Uchralism <uchral.champ@gmail.com>
2026-03-09 22:24:26 +0800

    build edit last

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,53 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+            },
+          },
+        ],
+        as: "*.js",
+      },
+    },
+  },
+  // output: "standalone",
 
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: any) => rule?.test?.test?.(".svg"));
+    config.module.rules.push({
+      test: /\.svg$/,
+      // issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-    config.module.rules.push(
-      // өмнөх asset loader-ийг ?url үед ашиглана
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      // бусад svg-г React component болгоно
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule?.issuer,
-        resourceQuery: {
-          not: [...(fileLoaderRule?.resourceQuery?.not || []), /url/],
-        },
-        use: ["@svgr/webpack"],
-      }
-    );
+    // // const fileLoaderRule = config.module.rules.find((rule: any) => rule?.test?.test?.(".svg"));
 
-    if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
+    // // config.module.rules.push(
+    // //   // өмнөх asset loader-ийг ?url үед ашиглана
+    // //   {
+    // //     ...fileLoaderRule,
+    // //     test: /\.svg$/i,
+    // //     resourceQuery: /url/,
+    // //   },
+    // //   // бусад svg-г React component болгоно
+    // //   {
+    // //     test: /\.svg$/i,
+    // //     issuer: fileLoaderRule?.issuer,
+    // //     resourceQuery: {
+    // //       not: [...(fileLoaderRule?.resourceQuery?.not || []), /url/],
+    // //     },
+    // //     use: ["@svgr/webpack"],
+    // //   }
+    // // );
+
+    // // if (fileLoaderRule) {
+    // //   fileLoaderRule.exclude = /\.svg$/i;
+    // }
 
     return config;
   },
