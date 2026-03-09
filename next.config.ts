e19f300167206418c0ea72c349b738d6380c72ee
baazka <baazka0705@gmail.com>
2026-03-09 14:19:51 +0800

    -webpack svg

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+    const fileLoaderRule = config.module.rules.find((rule: any) => rule?.test?.test?.(".svg"));
+
+    config.module.rules.push(
+      // өмнөх asset loader-ийг ?url үед ашиглана
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      // бусад svg-г React component болгоно
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule?.issuer,
+        resourceQuery: {
+          not: [...(fileLoaderRule?.resourceQuery?.not || []), /url/],
+        },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
     return config;
   },
