a44ae428d3ca5ca74cd7bbe27c8af094c018552a
baazka <baazka0705@gmail.com>
2026-03-09 15:29:31 +0800

    -test url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,6 +9,10 @@ const pool = new Pool({
   port: parseInt(process.env.PGPORT || "5432"),
 });
 
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DATABASE_URL =", process.env.DATABASE_URL?.replace(/\/\/.*:.*@/, "//***:***@"));
+
 pool.on("connect", () => {
   console.log("Connected to the database");
 });
