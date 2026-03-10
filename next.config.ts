8081efc7dfa63998c49abb94fae7ca8e0e70f222
baazka <baazka0705@gmail.com>
2026-03-10 10:36:41 +0800

    -Clear

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   turbopack: {
     rules: {
       "*.svg": {
@@ -16,38 +18,36 @@ const nextConfig: NextConfig = {
       },
     },
   },
-  output: "standalone",
 
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      // issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+    const fileLoaderRule = config.module.rules.find((rule: any) => rule?.test?.test?.(".svg"));
 
-    // // const fileLoaderRule = config.module.rules.find((rule: any) => rule?.test?.test?.(".svg"));
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // import logoUrl from "./logo.svg?url"
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: {
+          not: [/url/],
+        },
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+            },
+          },
+        ],
+      }
+    );
 
-    // // config.module.rules.push(
-    // //   // өмнөх asset loader-ийг ?url үед ашиглана
-    // //   {
-    // //     ...fileLoaderRule,
-    // //     test: /\.svg$/i,
-    // //     resourceQuery: /url/,
-    // //   },
-    // //   // бусад svg-г React component болгоно
-    // //   {
-    // //     test: /\.svg$/i,
-    // //     issuer: fileLoaderRule?.issuer,
-    // //     resourceQuery: {
-    // //       not: [...(fileLoaderRule?.resourceQuery?.not || []), /url/],
-    // //     },
-    // //     use: ["@svgr/webpack"],
-    // //   }
-    // // );
-
-    // // if (fileLoaderRule) {
-    // //   fileLoaderRule.exclude = /\.svg$/i;
-    // }
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
     return config;
   },
