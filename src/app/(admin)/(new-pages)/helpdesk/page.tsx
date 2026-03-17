2c5c716658bb562ea4b4c1d54aa18b2ff682784f
Uchralism <uchral.champ@gmail.com>
2026-03-17 14:49:33 +0800

    HD

---
+++ src/app/(admin)/(new-pages)/helpdesk/page.tsx
@@ -0,0 +1,12 @@
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import React from "react";
+import TaskList from "@/components/task/task-list/TaskList";
+
+export default function Task() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Тусламжийн хүсэлт" />
+      {/* <TaskList /> */}
+    </div>
+  );
+}
