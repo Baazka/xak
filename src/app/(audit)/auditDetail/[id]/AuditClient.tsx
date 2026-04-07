f29c48d3a798e9cf6f940d758d8e985422675961
baazka <baazka0705@gmail.com>
2026-04-07 14:35:15 +0800

    -form104

--- src/app/(audit)/auditDetail/[id]/AuditClient.tsx
+++ src/app/(audit)/auditDetail/[id]/AuditClient.tsx
@@ -15,27 +15,9 @@ export type FormItem = {
 const forms: FormItem[] = [
   { id: "m01", short: "М01", full: "Маягт 01" },
   { id: "m02", short: "М02", full: "Маягт 02" },
+  { id: "m03", short: "М03", full: "Маягт 03" },
   { id: "form1", short: "М1", full: "Маягт 1" },
   { id: "form2", short: "М2", full: "Маягт 2" },
-  { id: "form3", short: "М3", full: "Маягт 3" },
-  { id: "form4", short: "М4", full: "Маягт 4" },
-  { id: "form5", short: "М5", full: "Маягт 5" },
-  { id: "form6", short: "М6", full: "Маягт 6" },
-  { id: "form7", short: "М7", full: "Маягт 7" },
-  { id: "form8", short: "М8", full: "Маягт 8" },
-  { id: "form9", short: "М9", full: "Маягт 9" },
-  { id: "form10", short: "М10", full: "Маягт 10" },
-
-  { id: "form11", short: "М11", full: "Маягт 11" },
-  { id: "form12", short: "М12", full: "Маягт 12" },
-  { id: "form13", short: "М13", full: "Маягт 13" },
-  { id: "form14", short: "М14", full: "Маягт 14" },
-  { id: "form15", short: "М15", full: "Маягт 15" },
-  { id: "form16", short: "М16", full: "Маягт 16" },
-  { id: "form17", short: "М17", full: "Маягт 17" },
-  { id: "form18", short: "М18", full: "Маягт 18" },
-  { id: "form19", short: "М19", full: "Маягт 19" },
-  { id: "form20", short: "М20", full: "Маягт 20" },
 ];
 
 export default function AuditDetailClient({ auditId }: { auditId: number }) {
