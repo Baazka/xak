c15f3a390d072e4d33b4c668fa1b613c8c70d08a
Uchralism <uchral.champ@gmail.com>
2026-04-08 22:02:52 +0800

    form 208 209 api

---
+++ src/app/api/audit/form208/route.ts
@@ -0,0 +1,162 @@
+import { NextRequest, NextResponse } from "next/server";
+import db from "@/lib/db";
+import { withAuth } from "@/lib/withAuth";
+import { requirePermission } from "@/lib/requirePermission";
+import { JwtPayload } from "@/lib/jwtPayload";
+
+export const GET = withAuth(async (req: NextRequest, user: JwtPayload) => {
+  //requirePermission(user.permissions, ["user.read"]);
+
+  const userId = user.id;
+  const sp = new URL(req.url).searchParams;
+  const audId = sp.get("aud_id");
+  if (!audId) {
+    return NextResponse.json({ error: "Audit ID is required" }, { status: 400 });
+  }
+
+  const client = await db.connect();
+  try {
+    const isExistRes = await client.query(
+      `select form_id from audit_forms where form_aud_id = $1 and form_list_id = 7 limit 1`,
+      [audId]
+    );
+    if (!isExistRes.rows[0]) {
+      const newFormRes = await client.query(
+        `INSERT INTO audit_forms (form_aud_id, form_list_id, form_status_id) VALUES ($1, 7, 1) RETURNING form_id`,
+        [audId]
+      );
+
+      const formId = newFormRes.rows[0].form_id;
+
+      await client.query(
+        `INSERT INTO audit_form_actions (action_form_id, action_status_id, action_date, action_by) VALUES ($1, 1, current_timestamp, $2)`,
+        [formId, userId]
+      );
+
+      const indListRes = await client.query(
+        `SELECT ind_id as str_ind_id, null as str_ind_value, ind_label FROM ref_indicator i where ind_form_list_id = 7 order by i.ind_id`
+      );
+
+      for (const row of indListRes.rows) {
+        const indId = row.str_ind_id;
+        const strategyRes = await client.query(
+          `INSERT INTO audit_strategy (str_form_id, str_ind_id) VALUES ($1, $2) returning str_id`,
+          [formId, indId]
+        );
+      }
+    }
+    const formResLast = await client.query(
+      `SELECT form_id FROM audit_forms WHERE form_aud_id = $1 AND form_list_id = 7`,
+      [audId]
+    );
+    const formId = formResLast.rows[0].form_id;
+
+    const formDataRes = await client.query(
+      `
+        select 
+        f.form_id,
+        f.form_aud_id,
+        f.form_list_id,
+        af.form_code,
+        af.form_name,
+        f.form_status_id,
+        s.status_label form_status_name,
+        f.form_description,
+        f.form_file_id
+        from audit_forms f
+        join ref_audit_form af on f.form_list_id = af.form_id
+        join ref_form_status s on f.form_status_id = s.status_id
+        where f.form_id = $1`,
+      [formId]
+    );
+
+    const dataRes = await client.query(
+      `
+      select 
+        s.str_id,
+        s.str_form_id,
+        s.str_ind_id,
+        i.ind_label str_ind_label,
+        s.str_ind_value
+        from audit_strategy s
+        join ref_indicator i on s.str_ind_id = i.ind_id
+        where s.str_form_id = $1
+    `,
+      [formId]
+    );
+
+    return NextResponse.json(
+      { formData: formDataRes.rows[0], data: dataRes.rows, form_id: formId },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("DB Error:", err);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  } finally {
+    client.release();
+  }
+});
+
+export const POST = withAuth(async (req: NextRequest, user: JwtPayload) => {
+  //requirePermission(user.permissions, ["user.create"]);
+
+  const body = await req.json();
+  const userId = user.id;
+  // Check Insert or Update
+  const audId = body.aud_id;
+  const formId = body.form_id;
+  const statusId = body.status_id;
+  const formDescription = body.form_description;
+  const formFileId = body.form_file_id;
+
+  const strData: { str_id: number; str_ind_id: number; str_ind_value: string }[] = body.strData;
+
+  if (!statusId || !audId || !formId || !strData || !Array.isArray(strData)) {
+    return NextResponse.json({ error: "Мэдээлэл бүрэн оруулна уу" }, { status: 400 });
+  }
+
+  const client = await db.connect();
+  try {
+    await client.query("BEGIN");
+    // UPDATE audit_forms
+    await client.query(
+      `
+        UPDATE audit_forms
+        set form_status_id = $1,
+            form_description = $2,
+            form_file_id = $3
+        where form_id = $4
+      `,
+      [statusId, formDescription, formFileId, formId]
+    );
+    // INSERT audit_form_actions
+    await client.query(
+      `INSERT INTO audit_form_actions (action_form_id, action_status_id, action_date, action_by) VALUES ($1, $2, current_timestamp, $3)`,
+      [formId, statusId, userId]
+    );
+
+    // UPDATE AUDIT_STRATEGY
+    for (const str of strData) {
+      const { str_id, str_ind_id, str_ind_value } = str;
+
+      await client.query(
+        `
+          UPDATE audit_strategy
+          set str_ind_value = $1
+          where str_id = $2 and str_ind_id = $3
+        `,
+        [str_ind_value, str_id, str_ind_id]
+      );
+    }
+
+    await client.query("COMMIT");
+    return NextResponse.json({ message: "Strategy updated successfully" }, { status: 201 });
+  } catch (err: any) {
+    await client.query("ROLLBACK").catch(() => {});
+    console.error("Strategy update error:", err);
+
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  } finally {
+    client.release();
+  }
+});
--- src/app/api/audit/form209/route.ts
+++ src/app/api/audit/form209/route.ts
@@ -18,12 +18,12 @@ export const GET = withAuth(async (req: NextRequest, user: JwtPayload) => {
 
   try {
     const formRes = await client.query(
-      `SELECT form_id FROM audit_forms WHERE form_aud_id = $1 AND form_list_id = 5`,
+      `SELECT form_id FROM audit_forms WHERE form_aud_id = $1 AND form_list_id = 8`,
       [audId]
     );
     if (!formRes.rows[0]) {
       const newFormRes = await client.query(
-        `INSERT INTO audit_forms (form_aud_id, form_list_id, form_status_id) VALUES ($1, 5, 1) RETURNING form_id`,
+        `INSERT INTO audit_forms (form_aud_id, form_list_id, form_status_id) VALUES ($1, 8, 1) RETURNING form_id`,
         [audId]
       );
       const NewformId = newFormRes.rows[0].form_id;
@@ -34,7 +34,7 @@ export const GET = withAuth(async (req: NextRequest, user: JwtPayload) => {
       );
     }
     const formResLast = await client.query(
-      `SELECT form_id FROM audit_forms WHERE form_aud_id = $1 AND form_list_id = 5`,
+      `SELECT form_id FROM audit_forms WHERE form_aud_id = $1 AND form_list_id = 8`,
       [audId]
     );
     const formId = formResLast.rows[0].form_id;
@@ -49,7 +49,8 @@ export const GET = withAuth(async (req: NextRequest, user: JwtPayload) => {
         af.form_name,
         f.form_status_id,
         s.status_label form_status_name,
-        f.form_description
+        f.form_description,
+        f.form_file_id
         from audit_forms f
         join ref_audit_form af on f.form_list_id = af.form_id
         join ref_form_status s on f.form_status_id = s.status_id
@@ -60,22 +61,23 @@ export const GET = withAuth(async (req: NextRequest, user: JwtPayload) => {
     const dataRes = await client.query(
       `
       select 
-        meeting_id,
-        meeting_aud_id,
-        meeting_form_id,
-        meeting_type_id,
-        mt.type_label meeting_type_name,
-        to_char(meeting_date, 'YYYY.MM.DD')::text as meeting_date,
-        to_char(meeting_time, 'HH24:MI')::text as meeting_time,
-        meeting_place,
-        meeting_scope,
-        meeting_file_id
-        from audit_meetings t
-        join ref_meeting_type mt on t.meeting_type_id = mt.type_id
-        where meeting_aud_id = $1 and meeting_form_id = $2
-      ORDER BY meeting_id DESC
+        p.plan_id,
+        p.plan_form_id,
+        p.plan_type_id,
+        t.type_label plan_type_name,
+        to_char(plan_date,'YYYY.MM.DD') plan_date,
+        to_char(plan_comp_date,'YYYY.MM.DD') plan_comp_date,
+        p.plan_description,
+        p.plan_user_id,
+        u.user_firstname,
+        u.user_phone,
+        p.plan_file_id
+        from audit_plan p 
+        join ref_plan_type t on p.plan_type_id = t.type_id
+        join reg_users_new u on p.plan_user_id = u.user_id
+        where p.plan_form_id = $1
     `,
-      [audId, formId]
+      [formId]
     );
 
     return NextResponse.json(
@@ -98,69 +100,42 @@ export const POST = withAuth(async (req: NextRequest, user: JwtPayload) => {
   //requirePermission(user.permissions, ["user.create"]);
 
   const body = await req.json();
-  console.log("Meeting BODY ", body);
   const userId = user.id;
   const audId = body.aud_id;
   const formId = body.form_id;
-  const meetingId = body.meeting_id;
-  const meetingTypeId = body.meeting_type_id;
-  const meetingDate = body.meeting_date;
-  const meetingTime = body.meeting_time;
-  const meetingPlace = body.meeting_place;
-  const meetingScope = body.meeting_scope;
-  const meetingFileId = body.meeting_file_id;
+  const planId = body.plan_id;
+  const planTypeId = body.plan_type_id;
+  const planDate = body.plan_date;
+  const planCompDate = body.plan_comp_date;
+  const planDescription = body.plan_description;
+  const planFileId = body.plan_file_id;
 
-  if (
-    !meetingTypeId ||
-    !meetingDate ||
-    !meetingTime ||
-    !meetingPlace ||
-    !meetingScope ||
-    !meetingFileId
-  ) {
-    return NextResponse.json({ error: "Хүсэлтийн мэдээлэл бүрэн оруулна уу" }, { status: 400 });
+  if (!planTypeId || !planFileId || !planDate) {
+    return NextResponse.json({ error: "Төлөвлөгөөний мэдээлэл бүрэн оруулна уу" }, { status: 400 });
   }
 
   const client = await db.connect();
 
   try {
-    if (meetingId) {
-      // audit_meetings update
+    if (planId) {
+      // audit_plan update
       await client.query(
-        `UPDATE audit_meetings SET meeting_type_id = $1, meeting_place = $2, meeting_date = $3, meeting_time = $4, meeting_scope = $5, meeting_file_id = $6 WHERE meeting_id = $7`,
-        [
-          meetingTypeId,
-          meetingPlace,
-          meetingDate,
-          meetingTime,
-          meetingScope,
-          meetingFileId,
-          meetingId,
-        ]
+        `UPDATE audit_plan SET plan_type_id = $1, plan_file_id = $2, plan_date = $3, plan_comp_date = $4, plan_description = $5, plan_user_id = $6 WHERE plan_id = $7`,
+        [planTypeId, planFileId, planDate, planCompDate, planDescription, userId, planId]
       );
-      return NextResponse.json({ message: "Meeting updated successfully" }, { status: 201 });
+      return NextResponse.json({ message: "Audit plan updated successfully" }, { status: 201 });
     } else {
-      // audit_meetings insert
-      const meetingRes = await client.query(
-        `INSERT INTO audit_meetings (meeting_aud_id, meeting_form_id, meeting_type_id, meeting_place, meeting_date, meeting_time, meeting_scope, meeting_file_id, created_by, created_date)
-           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, current_timestamp) RETURNING meeting_id`,
-        [
-          audId,
-          formId,
-          meetingTypeId,
-          meetingPlace,
-          meetingDate,
-          meetingTime,
-          meetingScope,
-          meetingFileId,
-          userId,
-        ]
+      // audit_plan insert
+      const planRes = await client.query(
+        `INSERT INTO audit_plan (plan_form_id, plan_type_id, plan_file_id, plan_date, plan_comp_date, plan_description, plan_user_id)
+           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, current_timestamp) RETURNING plan_id`,
+        [formId, planTypeId, planFileId, planDate, planCompDate, planDescription, userId]
       );
-      return NextResponse.json({ meeting_id: meetingRes.rows[0].meeting_id }, { status: 200 });
+      return NextResponse.json({ plan_id: planRes.rows[0].plan_id }, { status: 200 });
     }
   } catch (err: any) {
     await client.query("ROLLBACK").catch(() => {});
-    console.error("Create meeting error:", err);
+    console.error("Create plan error:", err);
 
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   } finally {
@@ -173,9 +148,9 @@ export const DELETE = withAuth(async (req: NextRequest, user: JwtPayload) => {
 
   const body = await req.json();
 
-  const meetingId = body.meeting_id;
-  if (!meetingId) {
-    return NextResponse.json({ error: "Meeting ID is required" }, { status: 400 });
+  const planId = body.plan_id;
+  if (!planId) {
+    return NextResponse.json({ error: "Plan ID is required" }, { status: 400 });
   }
 
   const client = await db.connect();
@@ -183,14 +158,14 @@ export const DELETE = withAuth(async (req: NextRequest, user: JwtPayload) => {
   try {
     await client.query("BEGIN");
 
-    // audit_meetings delete
-    await client.query(`DELETE FROM audit_meetings WHERE meeting_id = $1`, [meetingId]);
+    // audit_plan delete
+    await client.query(`DELETE FROM audit_plan WHERE plan_id = $1`, [planId]);
 
     await client.query("COMMIT");
-    return NextResponse.json({ message: "Meeting deleted successfully" }, { status: 200 });
+    return NextResponse.json({ message: "Audit plan deleted successfully" }, { status: 200 });
   } catch (err: any) {
     await client.query("ROLLBACK").catch(() => {});
-    console.error("Delete meeting error:", err);
+    console.error("Delete plan error:", err);
 
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   } finally {
