de383b23ec789cc9e19c469410b4cd4cc973bc72
baazka <baazka0705@gmail.com>
2026-04-13 15:51:26 +0800

    -auditheader style

--- src/app/(audit)/auditDetail/[id]/components/AuditOrgCard.tsx
+++ src/app/(audit)/auditDetail/[id]/components/AuditOrgCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ArrowLeft, ChevronDown } from "lucide-react";
+import { ArrowLeft, ChevronDown, Edit } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -69,6 +69,8 @@ export default function AuditOrgCard({
 
         setHeaderData(data.audOrgResult);
         setTeamData(data.audTeamResult || []);
+
+        console.log(teamData, "teamData");
       } catch (err) {
         console.error(err);
       } finally {
@@ -124,115 +126,109 @@ export default function AuditOrgCard({
       </div>
 
       {openOrg && (
-        <div className="absolute z-30 left-0 right-0 top-full mt-2 rounded-2xl border bg-white shadow-lg p-4">
-          <div className="grid grid-auto-flow:column grid-cols-3 gap-4">
-            <div className="pl-4">
-              <div className="flex items-center gap-1 mb-2">
-                <p className="text-sm text-gray-800">Байгууллагын нэр:</p>
-                <input type="text" value={headerData?.org_legal_name ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mb-2">
-                <p className="text-sm text-gray-800">Байгууллагын регистр:</p>
-                <input type="text" value={headerData?.org_regno ?? ""} readOnly />
-              </div>
+        <div className="absolute left-0 right-0 top-full z-30 mt-2 rounded-2xl border bg-white p-4 shadow-lg">
+          <div className="grid grid-cols-3 gap-4 text-sm">
+            <div className="pl-4 grid grid-cols-[180px_1fr] gap-y-2">
+              <p className="text-gray-500">Байгууллагын нэр:</p>
+              <span>{headerData?.org_legal_name ?? "-"}</span>
+
+              <p className="text-gray-500">Байгууллагын регистр:</p>
+              <span>{headerData?.org_regno ?? "-"}</span>
             </div>
-            <div className="border-l border-gray-300 pl-4">
-              <div className="flex items-center gap-1 mb-2">
-                <p className="text-sm text-gray-800">Удирдлагын нэр:</p>
-                <input type="text" value={headerData?.org_head_name ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mb-2">
-                <p className="text-sm text-gray-800">Удирдлагын утас:</p>
-                <input type="text" value={headerData?.org_head_phone ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1">
-                <p className="text-sm text-gray-800">Удирдлагын мэйл:</p>
-                <input type="text" value={headerData?.org_head_email ?? ""} readOnly />
-              </div>
+
+            <div className="border-l border-gray-300 pl-4 grid grid-cols-[180px_1fr] gap-y-2">
+              <p className="text-gray-500">Удирдлагын нэр:</p>
+              <span>{headerData?.org_head_name ?? "-"}</span>
+
+              <p className="text-gray-500">Удирдлагын утас:</p>
+              <span>{headerData?.org_head_phone ?? "-"}</span>
+
+              <p className="text-gray-500">Удирдлагын мэйл:</p>
+              <span>{headerData?.org_head_email ?? "-"}</span>
             </div>
-            <div className="border-l border-gray-300 pl-4">
-              <div className="flex items-center gap-1 mb-2">
-                <p className="text-sm text-gray-800">Нягтлан бодогчийн нэр:</p>
-                <input type="text" value={headerData?.org_acc_name ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mb-2">
-                <p className="text-sm text-gray-800">Нягтлан бodогчийн утас:</p>
-                <input type="text" value={headerData?.org_acc_phone ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mb-2">
-                <p className="text-sm text-gray-800">Нягтлан бодогчийн мэйл:</p>
-                <input type="text" value={headerData?.org_acc_email ?? ""} readOnly />
-              </div>
+
+            <div className="border-l border-gray-300 pl-4 grid grid-cols-[180px_1fr] gap-y-2">
+              <p className="text-gray-500">Нягтлан бодогчийн нэр:</p>
+              <span>{headerData?.org_acc_name ?? "-"}</span>
+
+              <p className="text-gray-500">Нягтлан бодогчийн утас:</p>
+              <span>{headerData?.org_acc_phone ?? "-"}</span>
+
+              <p className="text-gray-500">Нягтлан бодогчийн мэйл:</p>
+              <span>{headerData?.org_acc_email ?? "-"}</span>
             </div>
           </div>
         </div>
       )}
 
       {openAudit && (
-        <div className="absolute left-0 right-0 top-full mt-2 rounded-2xl border bg-white shadow-lg p-4">
-          <div className="grid grid-auto-flow:column grid-cols-2 gap-4">
-            <div className="border-r-2 border-gray-300 pr-4">
-              <div className="flex items-center justify-between">
-                <div>
-                  <p className="text-sm text-gray-950 font-semibold">Аудитын үндсэн мэдээлэл</p>
-                </div>
-                <div>
-                  <button
-                    type="button"
-                    className="rounded-md bg-blue-600 px-3 py-2 text-sm text-white hover:bg-blue-700"
-                  >
-                    засах
-                  </button>
-                </div>
-              </div>
-              <div className="flex items-center gap-1 mt-2">
-                <p className="text-sm text-gray-950">Аудитын нэр:</p>
-                <input type="text" value={headerData?.aud_name ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mt-2">
-                <p className="text-sm text-gray-950">Аудитын жил:</p>
-                <input type="text" value={headerData?.aud_year ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mt-2">
-                <p className="text-sm text-gray-950">Аудит дуусах хугацаа:</p>
-                <input type="text" value={headerData?.aud_end_date ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mt-2">
-                <p className="text-sm text-gray-950">Аудит дуусах хугацаа:</p>
-                <input type="text" value={headerData?.aud_end_date ?? ""} readOnly />
-              </div>
-              <div className="flex items-center gap-1 mt-2">
-                <p className="text-sm text-gray-950">Аудитын гэрээ:</p>
-                <a href="" className="text-blue-600 hover:underline">
-                  Гэрээ файл
-                </a>
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between gap-3 mt-2">
-                <div className="flex items-center gap-1 justify-center">
-                  <p className="text-sm text-gray-950 font-semibold">Аудитын багийн мэдээлэл</p>
-                </div>
-                <div className="flex items-center gap-1 justify-center">
-                  <button
-                    type="button"
-                    className="rounded-md bg-blue-600 px-3 py-2 text-sm text-white hover:bg-blue-700"
-                  >
-                    засах
-                  </button>
-                </div>
+        <div className="absolute left-0 right-0 top-full z-30 mt-2 rounded-2xl border bg-white p-4 shadow-lg">
+          <div className="grid grid-cols-3 gap-4">
+            <div className="pl-4">
+              <div className="mb-3 flex items-center justify-between">
+                <p className="text-sm font-semibold text-gray-800">Аудитын үндсэн мэдээлэл</p>
+                <Edit className="h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700" />
               </div>
 
-              {teamData?.map((member) => (
-                <div key={member.team_id} className="grid items-center gap-1 mt-2 grid-cols-2">
-                  <p className="text-sm text-gray-950 text-end">{member.role_text}:</p>
-                  <input
-                    type="text"
-                    value={member.user_firstname + " (" + member.user_phone + ")"}
-                    readOnly
-                  />
+              <div className="space-y-2 text-sm">
+                <div className="grid grid-cols-[130px_1fr] items-start">
+                  <p className="text-gray-500">Аудитын нэр:</p>
+                  <span>{headerData?.aud_name ?? "-"}</span>
                 </div>
-              ))}
+
+                <div className="grid grid-cols-[130px_1fr] items-start">
+                  <p className="text-gray-500">Аудитын жил:</p>
+                  <span>{headerData?.aud_year ?? "-"}</span>
+                </div>
+
+                <div className="grid grid-cols-[130px_1fr] items-start">
+                  <p className="text-gray-500">Аудитын огноо:</p>
+                  <span>
+                    {headerData?.aud_begin_date ?? "-"} - {headerData?.aud_end_date ?? "-"}
+                  </span>
+                </div>
+                <div className="grid grid-cols-[130px_1fr] items-start">
+                  <p className="text-gray-500">Аудитын гэрээ:</p>
+                  <a href="" className="text-blue-600 hover:underline">
+                    Гэрээ файл
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-2 border-l border-gray-300 pl-4">
+              <div className="mb-3 flex items-center justify-between">
+                <p className="text-sm font-semibold text-gray-800">Аудитын багийн мэдээлэл</p>
+                <Edit className="h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700" />
+              </div>
+
+              <div className="grid grid-cols-2 gap-4">
+                <div className="space-y-2 text-sm">
+                  {teamData
+                    ?.filter((a) => a.team_role_id !== 6)
+                    .map((member) => (
+                      <div key={member.team_id} className="grid grid-cols-[150px_1fr] items-start">
+                        <p className="text-gray-500">{member.role_text}:</p>
+                        <span>
+                          {member.user_firstname ?? "-"} ({member.user_phone ?? "-"})
+                        </span>
+                      </div>
+                    ))}
+                </div>
+
+                <div className="space-y-2 text-sm">
+                  {teamData
+                    ?.filter((a) => a.team_role_id === 6)
+                    .map((member) => (
+                      <div key={member.team_id} className="grid grid-cols-[100px_1fr] items-start">
+                        <p className="text-gray-500">{member.role_text}:</p>
+                        <span>
+                          {member.user_firstname ?? "-"} ({member.user_phone ?? "-"})
+                        </span>
+                      </div>
+                    ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
