131b05593316b6ca103b979207d4f5ecf9bf87ed
Uchralism <uchral.champ@gmail.com>
2026-05-05 16:36:05 +0800

    a

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -56,7 +56,6 @@ import CallIcon from "./call-icon.svg";
 import BoxMoving from "./box-tapped.svg";
 import BoxTapped from "./box-icon.svg";
 import TruckDelivery from "./truck-delivery.svg";
-import SendIcon from "./send.svg";
 
 export {
   BoxTapped,
@@ -117,5 +116,4 @@ export {
   ChevronUpIcon,
   ChatIcon,
   MoreDotIcon,
-  SendIcon,
 };
